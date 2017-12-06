904afaa6d25ca45995f67aca994bd01649509302
Le Lin <lelin999@gmail.com>
2017-12-05 20:02:47 -0800

    mathdojo complete

---
+++ fundamentals/math_class.rb
@@ -0,0 +1,20 @@
+class MathDojo
+  attr_reader :result
+
+  def initialize
+    @result = 0
+  end
+
+  def add *num
+    @result += num.flatten.reduce(0, :+)
+    self
+  end
+
+  def subtract *num
+    @result -= num.flatten.reduce(0, :+)
+    self
+  end
+
+end
+challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).result # => 4
+challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result # => 23.15
